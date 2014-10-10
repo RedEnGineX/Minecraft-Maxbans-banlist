d0a78cb22622e8604422d7312a67878c8ebff005
TheJedras <jedras@jedrasgame.pl>
2014-10-10 17:55:38 +0200

    Revert "First creation"
    This reverts commit 5f270e1ced817959f738d33a7cda0eae9527c301.

--- bany/inc/db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('db_host','>Enter host<');
-define('db_user','>DB user<');
-define('db_passwd','>Enter Passwd<');
-define('db_name','>Enter db name<');
-//Database Connection//
-@mysql_connect(db_host,db_user,db_passwd) or die("<b>ERROR:</b> ".mysql_error());
-@mysql_select_db(db_name);
-?>
